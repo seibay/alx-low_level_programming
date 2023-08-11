1a15113c44d1c7a6e283cb04804787d46039c875
seibay <seifelislambay@gmail.com>
2023-08-11 16:47:54 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+	{
+		putchar(alph);
+	}
+	putchar('\n');
+
+	return (0);
+}
