be886ade26e80877fd936e2458555c3ff176e0e0
seibay <seifelislambay@gmail.com>
2023-08-21 17:49:45 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to print its characters
+ */
+void puts2(char *str)
+{
+	int length;
+	int i;
+
+	if (str == NULL)
+	{
+		return;
+	}
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = 0; i < length; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
