0adbb1b25bfbdef7d9dfd842c1bf1ea6a071cbf8
seibay <seifelislambay@gmail.com>
2023-09-12 19:31:28 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - prints dog's name and age
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- * 
- * Description: A program that prints dog's name, age and owner
- */
 struct dog
 {
 	char *name;
