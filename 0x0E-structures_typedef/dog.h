7edc14b184871209abc99631be438e9e384a9123
seibay <seifelislambay@gmail.com>
2023-09-12 19:33:45 -0700

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: kach ma afsa
+ */
 struct dog
 {
 	char *name;
