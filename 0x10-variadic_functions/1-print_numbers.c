ee9d27040f722668486740a9110c5c5259ebe21b
seibay <seifelislambay@gmail.com>
2023-09-15 20:00:15 -0700

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,10 +17,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		return;
 	}
 
-	va_start(ap, separator, n);
+	va_start(ap, n);
 	while (i--)
 	{
 		printf("%d%s", va_arg(ap, int), i ? (separator ? separator : "") : "\n");
 	}
 	va_end(ap);
-}	
+}
