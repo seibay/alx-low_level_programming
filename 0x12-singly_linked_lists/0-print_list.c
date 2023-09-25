088f201ab5253f2728e97beb34a95ae3cdba57bd
seibay <seifelislambay@gmail.com>
2023-09-24 20:38:00 -0700

    Fixed an error in 0-print_list.c file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,19 +2,24 @@
 
 /**
  * print_list - prints all the elements of a list_t list
- * @h: data to be printed
+ * @h: a pointer to the head of the list
  *
- * Return: the number of nodes
+ * Return: the number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", (h->len), (h->str != NULL ? h->str : "(nil)"));
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
