12a40b188c8bec9073ec81a2075279b166fe965c
seibay <seifelislambay@gmail.com>
2023-10-03 10:19:31 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -41,7 +41,7 @@ int main(int ac, char **av)
 	if (from_fd)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 	if (to_fd)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, to_fd), exit(100);
 
 	return (EXIT_SUCCESS);
 }
