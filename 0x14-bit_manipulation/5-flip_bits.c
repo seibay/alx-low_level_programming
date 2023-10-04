7d14f8835ec26b5b89e66d0da96af956902a776f
seibay <seifelislambay@gmail.com>
2023-10-03 18:19:12 -0700

    Added 5-flip_bits.c file

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,11 +13,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int xor_result = n ^ m;
 	unsigned int count = 0;
 
-	while (xor_result > 0)
+	while (xor_result)
 	{
-		if (xor_result & 1)
+		if (xor_result & 1UL)
 			count++;
-		xor_result >>= 1;
+		xor_result = xor_result >> 1;
 	}
 	return (count);
 }
