92adf1dae28155dd50502d1b0f206c8089875f9f
seibay <seifelislambay@gmail.com>
2023-10-03 17:22:08 -0700

    Added 1-print_binary.c file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,9 +17,14 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] == '0' || b[i] == '1')
 		{
-			result = result << 1;
-			result += (b[i] - '0');
-			i++;
+			result = result << 1;/* if result contains  0010, it will be 0100,*/
+						/* which is 4 in decimal (multipies by 2 (*2)).*/
+						/* This op make room for the next binary digit*/
+						/* to be added */
+			result += (b[i] - '0');/* if b[i] = '1', '1' - '0' = 1 */
+						/* if b[i] = '0', '0' - '0' = 0 */
+						/* convert the binary digit char to int value */
+			i++;/*  moves to the next character in the string b */
 		}
 		else
 			return (0);
